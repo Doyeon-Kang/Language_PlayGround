a42a00a3a492ab0c92c3544e5417f923ca4dde2f
dobby <dy227474@gmail.com>
2022-06-28 14:08:13 +0900

    first project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 @click='messageChange'>{{ msg }}</h1>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  methods: {
+    messageChange() {
+      if(this.msg == 'Hello Vue!')
+        this.msg = 'Good!'
+      else 
+        this.msg = 'Hello Vue!'
+    }
+  },
+  watch: {
+    msg(newValue) {
+      alert("data 값을 " +newValue +'으로 변경합니다.')
+    }
+  }
+}
+</script>
+
+<style>
+  ul {
+    list-style: none;
+  }
+</style>
---
+++ src/components/Fruit.vue
@@ -0,0 +1,34 @@
+<template>
+    <section v-if="hasFruit">
+        <h1>Fruits</h1>
+        <ul>
+            <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+        </ul>
+    </section>
+    <section>
+        <h1>Reverse Fruit</h1>
+        <ul>
+            <li v-for="fruit in reverseFruit" :key="fruit">{{ fruit }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            fruits: ['Apple', 'Banana', 'Cherry']
+        }
+    },
+    computed: {
+        hasFruit() {
+            return this.fruits.length > 0
+        },
+        reverseFruit() {
+            return this.fruits.map(fruit => {
+                return fruit.split('').reverse().join('')
+            })
+        }
+    }
+}
+</script>
