1a34bd4dba0fd90b0f633f25810759c5ebd027fd
dobby <dy227474@gmail.com>
2022-06-30 15:15:40 +0900

    07클래스와 스타일 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 @click='messageChange'>{{ msg }}</h1>
+  <!-- Camelcase 표기법이 아닌 경우 ''따옴표를 사용하여 표시 -->
+  <h1 :class="{ active: isActive, 'text-danger': hasError }" @click='activate'>Hello!{{ isActive }}</h1>
+  
+  <!-- 인라인으로 표시 안 하고 객체를 직접 바인딩 할 수 있음 -->
+  <h2 :class="classObject" :style="{color, fontSize}" @click="changeStyle">Bye!</h2>
+  
+  <!-- 배열을 이용하여 여러 데이터를 클래스로 바인딩 할 수 있음 -->
+  <h3 :class="[activeClass, errorClass]">Nice!</h3>
 </template>
 
 <script>
@@ -7,27 +14,28 @@
 export default {
   data() {
     return {
-      msg: 'Hello Vue!'
+      isActive: false,
+      hasError: true,
+      classObject: {
+        active: true, 
+        'text-danger': false
+      },
+      activeClass: 'active',
+      errorClass: 'text-danger',
+      color: 'orange',
+      fontSize: '30px'
     }
   },
   methods: {
-    messageChange() {
-      if(this.msg == 'Hello Vue!')
-        this.msg = 'Good!'
-      else 
-        this.msg = 'Hello Vue!'
-    }
-  },
-  watch: {
-    msg(newValue) {
-      alert("data 값을 " +newValue +'으로 변경합니다.')
+    activate() {
+      this.isActive = true
+    },
+    changeStyle() {
+      this.color = 'red'
     }
   }
 }
 </script>
 
-<style>
-  ul {
-    list-style: none;
-  }
+<style scoped>
 </style>
