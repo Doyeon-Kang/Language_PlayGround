6265ecab0edcd6176bf3fb46e915b5648e8d917e
Kang Doyeon <kang.dy@whaleandsun.com>
2024-04-08 10:32:26 +0900

    project: apple-coding-example, content: > 2022 new react lecture - 3강

--- apple-coding-example/src/App.js
+++ apple-coding-example/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [title, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  // Q. 왜 state 써야하는가? 
+  // A. state 쓰던 html은 자동으로 재렌더링됨
+  // 즉, 자주 변경할 필요가 있는 변수는 useState로 선언하고 아니면 일반변수로 선언!
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
